f3576f3055673061fdbc9e2431b830a340bdefdc
Rye Terrell <wwwtyro@gmail.com>
2017-11-04 18:23:22 -0500

    topodb wip

--- tinyplanet/main.js
+++ tinyplanet/main.js
@@ -8,55 +8,24 @@ const SphereFPSCam = require('./sphere-fps-cam');
 const mat4 = glMatrix.mat4;
 const vec3 = glMatrix.vec3;
 
+const meshWorker = new Worker('build/bundled-worker.js');
+
 const cache = require('./cache');
 
 const earthRadius = 6371000; // meters
-const vScale = 4.0;
+const vScale = 2.0;
 
 main();
 
 async function main() {
 
   const nodeCache = cache(key => {
-    return `nodes/${key}.json`;
+    var url = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+    return `http://159.203.97.31/complete/${key}`;
   });
 
   const texture_img = await loadImage('texture.png');
-
   const color_img = await loadImage('earthcolor.jpg');
-
-  const color = (function() {
-    const w = color_img.width;
-    const h = color_img.height;
-    const canvas = document.createElement('canvas');
-    canvas.width = w;
-    canvas.height = h;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(color_img, 0, 0, w, h);
-    const data = ctx.getImageData(0, 0, w, h).data;
-
-    const pi = Math.PI;
-    const twopi = pi * 2;
-
-    const temp = [];
-    return function(p) {
-      p = vec3.normalize(temp, p);
-      const y = p[0];
-      const z = p[1]
-      const x = p[2];
-      const theta = Math.acos(z);
-      const phi = Math.atan2(y,x);
-      const i = clamp(Math.floor(w * (phi + pi)/twopi), 0, w - 1);
-      const j = clamp(Math.floor(h * theta/pi), 0, h - 1);
-      return [
-        data[(j * w + i) * 4 + 0]/255,
-        data[(j * w + i) * 4 + 1]/255,
-        data[(j * w + i) * 4 + 2]/255,
-      ];
-    }
-  })();
-
-
 
   const sphere = [
     quadtree.node( // Positive X
@@ -178,12 +147,21 @@ async function main() {
     const nodes = [];
     for (let root of sphere) {
       quadtree.traverse(root, function(node, depth) {
-        const radius = vec3.distance(
-          vec3.scale([], vec3.normalize([], node.c), earthRadius),
-          vec3.scale([], vec3.normalize([], node.se), earthRadius)
+        const radius = Math.max(
+          vec3.distance(
+            vec3.scale([], vec3.normalize([], node.c), earthRadius),
+            vec3.scale([], vec3.normalize([], node.se), earthRadius)
+          ),
+          vec3.distance(
+            vec3.scale([], vec3.normalize([], node.c), earthRadius),
+            vec3.scale([], vec3.normalize([], node.sw), earthRadius)
+          )
         );
         const dist = vec3.distance(p, vec3.scale([], vec3.normalize([], node.c), earthRadius));
-        if (dist > radius * 1) {
+        if (dist > radius * 2) {
+          if (node.id === 'px-' || node.id === 'py-' || node.id === 'pz-' || node.id === 'nx-' || node.id === 'ny-' || node.id === 'nz-') {
+            return false;
+          }
           const available = nodeCache.get(node.id);
           if (available) {
             nodes.push({
@@ -228,112 +206,31 @@ async function main() {
     return colorCache[p].slice();
   }
 
+  const terrainMeshesInFlight = {};
 
-  function buildMesh(node, enode) {
-    const res = enode.resolution;
-    const right = vec3.scale([], node.right, 2/res);
-    const up = vec3.scale([], node.up, 2/res);
-    const positions = [], colors = [], normals = [], uvs = [];
-    const bounds = {min: [Infinity, Infinity, Infinity], max: [-Infinity, -Infinity, -Infinity]};
-    for (let i = 0; i < res; i++) {
-      for (let j = 0; j < res; j++) {
-        let a = vec3.add([], vec3.add([], node.sw, vec3.scale([], right, i + 0)), vec3.scale([], up, j + 0));
-        let b = vec3.add([], vec3.add([], node.sw, vec3.scale([], right, i + 1)), vec3.scale([], up, j + 0));
-        let c = vec3.add([], vec3.add([], node.sw, vec3.scale([], right, i + 1)), vec3.scale([], up, j + 1));
-        let d = vec3.add([], vec3.add([], node.sw, vec3.scale([], right, i + 0)), vec3.scale([], up, j + 1));
+  const workerQueue = [];
 
-        const ma = vec3.scale([], vec3.normalize([], a), vScale * enode.elevations[i + 0][j + 0] + earthRadius);
-        const mb = vec3.scale([], vec3.normalize([], b), vScale * enode.elevations[i + 1][j + 0] + earthRadius);
-        const mc = vec3.scale([], vec3.normalize([], c), vScale * enode.elevations[i + 1][j + 1] + earthRadius);
-        const md = vec3.scale([], vec3.normalize([], d), vScale * enode.elevations[i + 0][j + 1] + earthRadius);
-
-        positions.push(ma.slice());
-        positions.push(mb.slice());
-        positions.push(mc.slice());
-        positions.push(ma.slice());
-        positions.push(mc.slice());
-        positions.push(md.slice());
-
-        vec3.min(bounds.min, bounds.min, ma);
-        vec3.min(bounds.min, bounds.min, mb);
-        vec3.min(bounds.min, bounds.min, mc);
-        vec3.min(bounds.min, bounds.min, md);
-        vec3.max(bounds.max, bounds.max, ma);
-        vec3.max(bounds.max, bounds.max, mb);
-        vec3.max(bounds.max, bounds.max, mc);
-        vec3.max(bounds.max, bounds.max, md);
-
-        let ab = vec3.normalize([], vec3.sub([], mb, ma));
-        let ac = vec3.normalize([], vec3.sub([], mc, ma));
-        let n = vec3.cross([], ab, ac);
-        normals.push(n);
-        normals.push(n);
-        normals.push(n);
-
-        let ad = vec3.normalize([], vec3.sub([], md, ma));
-        n = vec3.cross([], ac, ad);
-        normals.push(n);
-        normals.push(n);
-        normals.push(n);
-
-        const uva = [4 * (i + 0) / res, 4 * (j + 0) / res];
-        const uvb = [4 * (i + 1) / res, 4 * (j + 0) / res];
-        const uvc = [4 * (i + 1) / res, 4 * (j + 1) / res];
-        const uvd = [4 * (i + 0) / res, 4 * (j + 1) / res];
-
-        uvs.push(uva);
-        uvs.push(uvb);
-        uvs.push(uvc);
-        uvs.push(uva);
-        uvs.push(uvc);
-        uvs.push(uvd);
-
-        const ca = getColor(a);
-        const cb = getColor(b);
-        const cc = getColor(c);
-        const cd = getColor(d);
-        const cabc = vec3.scale([], vec3.add([], ca, vec3.add([], cb, cc)), 1/3);
-        const cacd = vec3.scale([], vec3.add([], ca, vec3.add([], cc, cd)), 1/3);
-        colors.push(cabc);
-        colors.push(cabc);
-        colors.push(cabc);
-        colors.push(cacd);
-        colors.push(cacd);
-        colors.push(cacd);
-      }
-    }
-
-    bounds.center = vec3.add([], bounds.min, vec3.scale([], vec3.sub([], bounds.max, bounds.min), 0.5));
-    for (let i = 0; i < positions.length; i++) {
-      vec3.sub(positions[i], positions[i], bounds.center);
-    }
-
-    // console.log(bounds);
-    //
-    // console.log(positions);
-    // assplode;
-
-    const bc = [];
-    for (let i = 0; i < positions.length/3; i++) {
-      bc.push([1,0,0]);
-      bc.push([0,1,0]);
-      bc.push([0,0,1]);
-    }
-
-    return {
-      offset: bounds.center,
-      positions: regl.buffer(positions),
-      colors: regl.buffer(colors),
-      uvs: regl.buffer(uvs),
-      normals: regl.buffer(normals),
-      bc: regl.buffer(bc),
-      count: positions.length,
-    }
+  meshWorker.onmessage = function(e) {
+    workerQueue.push(e.data);
   }
 
   function getMesh(node, enode) {
     if (!(node.id in terrainMeshes)) {
-      terrainMeshes[node.id] = buildMesh(node, enode);
+      if (node.id in terrainMeshesInFlight) {
+        return null;
+      }
+      if (Object.keys(terrainMeshesInFlight).length > 0) {
+        return null;
+      }
+      terrainMeshesInFlight[node.id] = true;
+      meshWorker.postMessage({
+        node: node,
+        enode: enode,
+        vScale: vScale,
+        earthRadius: earthRadius,
+      });
+      return null;
+      // terrainMeshes[node.id] = buildMesh(node, enode);
     }
     return terrainMeshes[node.id];
   }
@@ -392,14 +289,16 @@ async function main() {
       uniform vec3 light;
       varying vec3 vBC, vColor, vNormal;
       varying vec2 vUV;
+
+      vec3 saturate(vec3 c, float delta) {
+        float p = sqrt(c.r*c.r*0.299 + c.g*c.g*0.587 + c.b*c.b*0.114);
+        return p + delta * (c - p);
+      }
+
       void main() {
         float t = texture2D(texture, vUV).r;
         float l = 2.0 * clamp(dot(normalize(vNormal), normalize(light)), 0.25, 1.0);
-        if (any(lessThan(vBC, vec3(0.01)))) {
-          gl_FragColor = vec4(vColor * 0.5 * l, 1.0);
-        } else {
-          gl_FragColor = vec4(vColor * l * t * t, 1.0);
-        }
+        gl_FragColor = vec4(saturate(vColor * l * t * t, 1.0), 1.0);
       }
     `,
     attributes: {
@@ -424,8 +323,9 @@ async function main() {
     },
   });
 
+  let altitude = 1000;
   const camData = JSON.parse(localStorage.getItem('camData')) || {
-    position: [0,getElevation([0,1,0]) + 10 + earthRadius,0],
+    position: [0,getElevation([0,1,0]) + altitude + earthRadius,0],
     forward: [0,0,-1]
   };
   const cam = SphereFPSCam(camData.position, camData.forward);
@@ -434,28 +334,34 @@ async function main() {
     localStorage.setItem('camData', JSON.stringify(cam.dump()));
   }, 1000);
 
-  const arrows = {
+  const keyboard = {
     up: false,
     down: false,
     left: false,
     right: false,
     shift: false,
+    ascend: false,
+    descend: false,
   }
 
   window.addEventListener('keydown', function(e) {
-    if (e.which === 16) arrows.shift = true;
-    if (e.which === 87) arrows.up = true;
-    if (e.which === 83) arrows.down = true;
-    if (e.which === 65) arrows.left = true;
-    if (e.which === 68) arrows.right = true;
+    if (e.which === 69) keyboard.ascend = true;
+    if (e.which === 81) keyboard.descend = true;
+    if (e.which === 16) keyboard.shift = true;
+    if (e.which === 87) keyboard.up = true;
+    if (e.which === 83) keyboard.down = true;
+    if (e.which === 65) keyboard.left = true;
+    if (e.which === 68) keyboard.right = true;
   });
 
   window.addEventListener('keyup', function(e) {
-    if (e.which === 16) arrows.shift = false;
-    if (e.which === 87) arrows.up = false;
-    if (e.which === 83) arrows.down = false;
-    if (e.which === 65) arrows.left = false;
-    if (e.which === 68) arrows.right = false;
+    if (e.which === 69) keyboard.ascend = false;
+    if (e.which === 81) keyboard.descend = false;
+    if (e.which === 16) keyboard.shift = false;
+    if (e.which === 87) keyboard.up = false;
+    if (e.which === 83) keyboard.down = false;
+    if (e.which === 65) keyboard.left = false;
+    if (e.which === 68) keyboard.right = false;
   });
 
 
@@ -464,28 +370,53 @@ async function main() {
 
   function loop() {
 
+    const data = workerQueue.pop();
+    if (data) {
+      delete terrainMeshesInFlight[data.node.id];
+      terrainMeshes[data.node.id] = {
+        offset: data.offset,
+        positions: regl.buffer(data.positions),
+        colors: regl.buffer(data.colors),
+        uvs: regl.buffer(data.uvs),
+        normals: regl.buffer(data.normals),
+        bc: regl.buffer(data.bc),
+        count: data.count,
+      }
+    }
+
+
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
-    const speed = arrows.shift ? 10 : 1;
+    const speed = keyboard.shift ? 1 : 0.1;
 
-    if (arrows.up) {
-      cam.moveForward(4000 * speed);
+    if (keyboard.ascend) {
+      altitude *= 1.1;
     }
 
-    if (arrows.down) {
-      cam.moveForward(-4000 * speed);
+    if (keyboard.descend) {
+      altitude *= 0.9;
     }
 
-    if (arrows.left) {
-      cam.moveRight(-4000 * speed);
+    altitude = Math.min(Math.max(altitude, 1), 1000000);
+
+    if (keyboard.up) {
+      cam.moveForward(speed * altitude);
     }
 
-    if (arrows.right) {
-      cam.moveRight(4000 * speed);
+    if (keyboard.down) {
+      cam.moveForward(-speed * altitude);
     }
 
-    let e = 100000 + earthRadius + getElevation(cam.getPosition());
+    if (keyboard.left) {
+      cam.moveRight(-speed * altitude);
+    }
+
+    if (keyboard.right) {
+      cam.moveRight(speed * altitude);
+    }
+
+    let e = altitude + earthRadius + getElevation(cam.getPosition());
     let delta = e - vec3.length(cam.getPosition());
     cam.moveUp(delta * 0.1);
 
@@ -512,22 +443,30 @@ async function main() {
 
 
     const view = cam.getView(true);
-    const projection = mat4.perspective([], Math.PI/4, canvas.width/canvas.height, 10, 10000000);
-
-    const nodes = getAvailableNodes(cam.getPosition());
-
-    const meshes = [];
-
-    for (let node of nodes) {
-      meshes.push(getMesh(node.node, node.enode));
-    }
+    const projection = mat4.perspective([], Math.PI/4, canvas.width/canvas.height, 1, 10000000);
 
     regl.clear({
-      color: [110/255,163/255,209/255,1],
+      color: [65/255,168/255,255/255,1],
       depth: 1,
     });
 
-    for (let mesh of meshes) {
+    // First, render the root nodes.
+    const rootNodes = [];
+    for (let node of sphere) {
+      const available = nodeCache.get(node.id);
+      if (available) {
+        rootNodes.push({
+          node: node,
+          enode: available
+        });
+      }
+    }
+    const rootMeshes = [];
+    for (let node of rootNodes) {
+      rootMeshes.push(getMesh(node.node, node.enode));
+    }
+    for (let mesh of rootMeshes) {
+      if (mesh === null) continue;
       const translation = vec3.sub([], mesh.offset, cam.getPosition());
       const model = mat4.fromTranslation([], translation);
       render({
@@ -544,6 +483,41 @@ async function main() {
         count: mesh.count
       });
     }
+
+
+    // Now overdraw with whatever we can get.
+    regl.clear({
+      depth: 1,
+    });
+
+    const nodes = getAvailableNodes(cam.getPosition());
+
+    const meshes = [];
+
+    for (let node of nodes) {
+      meshes.push(getMesh(node.node, node.enode));
+    }
+
+    for (let mesh of meshes) {
+      if (mesh === null) continue;
+      const translation = vec3.sub([], mesh.offset, cam.getPosition());
+      const model = mat4.fromTranslation([], translation);
+      render({
+        model: model,
+        view: view,
+        projection: projection,
+        viewport: {x: 0, y: 0, width: canvas.width, height: canvas.height},
+        positions: mesh.positions,
+        normals: mesh.normals,
+        uvs: mesh.uvs,
+        colors: mesh.colors,
+        bc: mesh.bc,
+        light: vec3.normalize([], cam.getPosition()),
+        count: mesh.count
+      });
+    }
+
+    document.getElementById('info').innerText = `Altitude: ${Math.round(altitude)} meters`;
 
     requestAnimationFrame(loop);
   }
--- topodb/serve.js
+++ topodb/serve.js
@@ -9,10 +9,24 @@ const app = express();
 app.use(cors());
 
 const db = new sqlite3('nodes.sqlite3');
+const db_color = new sqlite3('colors.sqlite3');
 
 app.get('/tile/:id', function(req, res, next) {
   const row = db.prepare(`SELECT node FROM nodes WHERE id=?`).get(req.params.id);
   res.send(row.node);
+});
+
+app.get('/complete/:id', function(req, res, next) {
+  let node = db.prepare(`SELECT node FROM nodes WHERE id=?`).get(req.params.id);
+  let color = db_color.prepare(`SELECT node FROM colors WHERE id=?`).get(req.params.id);
+  node = JSON.parse(node.node);
+  color = JSON.parse(color.node);
+  res.send(JSON.stringify({
+    id: node.id,
+    resolution: node.resolution,
+    elevations: node.elevations,
+    color: color.colors,
+  }));
 });
 
 app.listen(process.argv[2] || true, function() {
